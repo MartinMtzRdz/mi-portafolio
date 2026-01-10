aaedee95b3b4eb91a62a11a683c3b123097f04d2
MartinMtzRdz <121314@insuco.edu.mx>
2026-01-09 23:06:47 -0600

    arreglo de flappy insuco

--- Flappy-Insuco-main/script.js
+++ Flappy-Insuco-main/script.js
@@ -2,8 +2,10 @@ class Game {
     constructor() {
         this.boardWidth = 360;
         this.boardHeight = 640;
+
         this.backgroundImg = new Image();
-        this.backgroundImg.src = "./Imagenes/fondomejorado.png";
+        this.backgroundImg.src = "./imagenes/fondomejorado.png";
+
         this.inputLocked = false;
         this.playBtn = document.getElementById("playBtn");
 
@@ -53,51 +55,46 @@ class Game {
 
     loadAssets() {
         this.birdImg = new Image();
-        this.birdImg.src = "./Imagenes/doberman.png";
+        this.birdImg.src = "./imagenes/doberman.png";
 
         this.topPipeImg = new Image();
-        this.topPipeImg.src = "./Imagenes/huesoalto.png";
+        this.topPipeImg.src = "./imagenes/huesoalto.png";
 
         this.bottomPipeImg = new Image();
-        this.bottomPipeImg.src = "./Imagenes/huesoalto.png";
+        this.bottomPipeImg.src = "./imagenes/huesoalto.png";
 
         this.playButtonImg = new Image();
-        this.playButtonImg.src = "./Imagenes/flappyBirdPlayButton.png";
+        this.playButtonImg.src = "./imagenes/flappyBirdPlayButton.png";
 
         this.flappyBirdTextImg = new Image();
-        this.flappyBirdTextImg.src = "./Imagenes/Logomejorado.png";
+        this.flappyBirdTextImg.src = "./imagenes/Logomejorado.png";
 
         this.gameOverImg = new Image();
-        this.gameOverImg.src = "./Imagenes/flappy-gameover.png";
+        this.gameOverImg.src = "./imagenes/flappy-gameover.png";
 
         this.jumpSound = new Audio("./Sonidos/salto.mp3");
         this.pointSound = new Audio("./Sonidos/punto.mp3");
         this.hitSound = new Audio("./Sonidos/golpe.mp3");
-
     }
 
     init() {
         window.onload = () => {
 
-const unlockAudio = () => {
-    if (this.jumpSound) {
-        this.jumpSound.play().then(() => {
-            this.jumpSound.pause();
-            this.jumpSound.currentTime = 0;
-            console.log("🔊 Audio desbloqueado");
-        }).catch((err) => {
-            console.warn("🔇 Audio bloqueado por navegador:", err);
-        });
-    }
+            const unlockAudio = () => {
+                if (this.jumpSound) {
+                    this.jumpSound.play().then(() => {
+                        this.jumpSound.pause();
+                        this.jumpSound.currentTime = 0;
+                    }).catch(() => {});
+                }
+                window.removeEventListener("click", unlockAudio);
+                window.removeEventListener("keydown", unlockAudio);
+                window.removeEventListener("touchstart", unlockAudio);
+            };
 
-    window.removeEventListener("click", unlockAudio);
-    window.removeEventListener("keydown", unlockAudio);
-    window.removeEventListener("touchstart", unlockAudio);
-};
-
-window.addEventListener("click", unlockAudio);
-window.addEventListener("keydown", unlockAudio);
-window.addEventListener("touchstart", unlockAudio);
+            window.addEventListener("click", unlockAudio);
+            window.addEventListener("keydown", unlockAudio);
+            window.addEventListener("touchstart", unlockAudio);
 
             this.board = document.getElementById("board");
             this.board.height = this.boardHeight;
@@ -106,27 +103,21 @@ window.addEventListener("touchstart", unlockAudio);
 
             document.addEventListener("keydown", this.handleKeyDown.bind(this));
             this.board.addEventListener("touchstart", this.handleTouch.bind(this), { passive: false });
+
             requestAnimationFrame(this.update.bind(this));
+
             this.playBtn.addEventListener("click", () => {
-                if (this.currentState === this.GAME_STATE.MENU){
+                if (this.currentState === this.GAME_STATE.MENU) {
                     this.startGame();
-
-                    this.playBtn.addEventListener("touchstart", (e) => {
-    e.preventDefault();
-    if (this.currentState === this.GAME_STATE.MENU) {
-        this.startGame();
-    }
-}, { passive: false });
-
                 }
             });
         };
+
         this.restartBtn = document.getElementById("restartBtn");
         this.restartBtn.addEventListener("click", () => {
             this.resetGame();
             this.currentState = this.GAME_STATE.MENU;
         });
-
     }
 
     update() {
@@ -161,43 +152,41 @@ window.addEventListener("touchstart", unlockAudio);
     }
 
     renderGame() {
-        if(this.backgroundImg.complete){
+        if (this.backgroundImg.complete) {
             this.context.drawImage(this.backgroundImg, 0, 0, this.boardWidth, this.boardHeight);
         }
+
         this.velocityY += this.gravity;
         this.bird.y = Math.max(this.bird.y + this.velocityY, 0);
-        this.context.drawImage(this.birdImg, this.bird.x, this.bird.y, this.bird.width, this.bird.height);
+
+        if (this.birdImg.complete) {
+            this.context.drawImage(this.birdImg, this.bird.x, this.bird.y, this.bird.width, this.bird.height);
+        }
 
         if (this.bird.y > this.board.height) {
             this.currentState = this.GAME_STATE.GAME_OVER;
         }
 
-        for (let i = 0; i < this.pipeArray. length; i++) {
+        for (let i = 0; i < this.pipeArray.length; i++) {
             const pipe = this.pipeArray[i];
             pipe.x += this.velocityX;
-            this.context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
 
-            if(i % 2 === 0 && !pipe.passed && this.bird.x > pipe.x + pipe.width) {
-                this.score += 1;
-                pipe.passed = true;
-                this.pointSound.play().catch(() => {})
+            if (pipe.img.complete) {
+                this.context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
             }
 
-            console.log(
-    "Debug Colisión →",
-    "Bird:", this.bird.x, this.bird.y,
-    "Pipe:", pipe.x, pipe.y,
-    "Overlap X:", (this.bird.x < pipe.x + pipe.width && this.bird.x + this.bird.width > pipe.x),
-    "Overlap Y:", (this.bird.y < pipe.y + pipe.height && this.bird.y + this.bird.height > pipe.y)
-  );
+            if (i % 2 === 0 && !pipe.passed && this.bird.x > pipe.x + pipe.width) {
+                this.score += 1;
+                pipe.passed = true;
+                this.pointSound.play().catch(() => {});
+            }
 
-                 if (this.detectCollision(this.bird, pipe)) {
-    console.warn("💥 Colisión detectada con tubo índice", i);
-    this.hitSound.play().catch(() => {});
-    this.currentState = this.GAME_STATE.GAME_OVER;
-    break;
-  }
-}
+            if (this.detectCollision(this.bird, pipe)) {
+                this.hitSound.play().catch(() => {});
+                this.currentState = this.GAME_STATE.GAME_OVER;
+                break;
+            }
+        }
 
         while (this.pipeArray.length > 0 && this.pipeArray[0].x < -this.pipeWidth) {
             this.pipeArray.shift();
@@ -210,44 +199,36 @@ window.addEventListener("touchstart", unlockAudio);
     }
 
     renderGameOver() {
-        if(this.backgroundImg.complete) {
+        if (this.backgroundImg.complete) {
             this.context.drawImage(this.backgroundImg, 0, 0, this.boardWidth, this.boardHeight);
         }
+
         if (this.gameOverImg.complete) {
-            let imgWidth = 400;
+            let imgWidth = 300;
             let imgHeight = 80;
             let x = (this.boardWidth - imgWidth) / 2;
             let y = this.boardHeight / 3;
 
             this.context.drawImage(this.gameOverImg, x, y, imgWidth, imgHeight);
 
-            let scoreText = `Your score: ${Math.floor(this.score)}`;
+            let scoreText = `Score: ${Math.floor(this.score)}`;
             if (this.score > this.highScore) {
                 this.highScore = this.score;
                 localStorage.setItem("flappyHighScore", this.highScore);
             }
-            this.context.fillStyle = "black";
-            this.context.font = "45px sans-serif";
-            this.context.textAlign = "center";
-            this.context.fillText(scoreText, this.boardWidth / 2, y + imgHeight + 50);
-            let highScoreText = `Record: ${Math.floor(this.highScore)}`;
-            this.context.fillText(highScoreText, this.boardWidth / 2, y + imgHeight + 100);
-            this.playBtn.style.display = "none";
 
-            this.inputLocked = true;
-            setTimeout(() => {
-                this.inputLocked = false;
-            }, 1000);
+            this.context.fillStyle = "black";
+            this.context.font = "30px sans-serif";
+            this.context.textAlign = "center";
+            this.context.fillText(scoreText, this.boardWidth / 2, y + imgHeight + 40);
         }
+
+        this.playBtn.style.display = "none";
+
         this.restartBtn.style.display = "block";
         this.restartBtn.style.left = "50%";
         this.restartBtn.style.transform = "translate(-50%, -500%)";
-        this.restartBtn.stlyle.top = `${this.boardHeight / 2 + 100}px`;
-
-        if (this.bird.y + this.bird.height >= this.boardHeight) {
-            this.hitSound.play().catch(() => {});
-            this.currentState = this.GAME_STATE.GAME_OVER;
-        }
+        this.restartBtn.style.top = `${this.boardHeight / 2 + 100}px`;
     }
 
     handleKeyDown(e) {
@@ -267,29 +248,11 @@ window.addEventListener("touchstart", unlockAudio);
     }
 
     handleTouch(e) {
-            if(this.inputLocked) return;
-
+        if (this.inputLocked) return;
         e.preventDefault();
 
-        const touch = e.touches[0];
-        const rect = this.board.getBoundingClientRect();
-        
-        const scaleX = this.board.width / rect.width;
-        const scaleY = this.board.height / rect.height;
-
-        const x = (touch.clientX - rect.left) * scaleX;
-        const y = (touch.clientY - rect.top) * scaleY;
-
         if (this.currentState === this.GAME_STATE.MENU) {
-
-            if (
-                x >= this.playButton.x &&
-                x <= this.playButton.x + this.playButton.width &&
-                y >= this.playButton.y &&
-                y <= this.playButton.y + this.playButton.height
-            ) {
-                this.startGame();
-            }
+            this.startGame();
         } else if (this.currentState === this.GAME_STATE.GAME_OVER) {
             this.resetGame();
             this.currentState = this.GAME_STATE.MENU;
@@ -318,7 +281,6 @@ window.addEventListener("touchstart", unlockAudio);
         this.bird.y = this.birdY;
         this.pipeArray = [];
         this.score = 0;
-
         this.restartBtn.style.display = "none";
     }
 
@@ -363,4 +325,3 @@ window.addEventListener("touchstart", unlockAudio);
 }
 
 const game = new Game();
-//prueba de cambio
